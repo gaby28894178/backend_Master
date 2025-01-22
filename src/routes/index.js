6d7233f9845c01f71a61f6c73d5569612bec6e9c
gaby288941787 <gabgabrielligabriel@gmail.com>
2025-01-22 01:00:23 -0300

    erc

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,10 @@ router.get('/status', (req, res) => {
     res.json({ message: 'API funcionando correctamente' });
 });
 
+router.get('/:id',(req,res)=>{
+    const {id}= req.params;
+    res.send("Hola su id de ingreso es el ID: {id}")
+})
+
+
 module.exports = router;
